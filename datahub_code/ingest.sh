c1acb3e251df609cb13214eb644c36930ab86ee7
ZeroGxMax <21020554@vnu.edu.vn>
2024-12-09 15:15:18 +0700

    Able to merge not test and test version

--- datahub_code/ingest.sh
+++ datahub_code/ingest.sh
@@ -1,2 +1,4 @@
-datahub ingest -c postgres.yaml
-datahub ingest -c dbt_test.yaml
+datahub ingest -c sources/postgres.yaml
+datahub ingest -c sources/dbt_test.yaml
+# datahub ingest -c sources/postgres_2.yaml
+# datahub ingest -c sources/dbt_test_2.yaml
